b50d610804043269f0649bb069adfb795067181c
Nedder3 <arij.dev@gmail.com>
2025-02-21 03:22:19 -0300

    fix masive assignment on role creation

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -2,14 +2,22 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Role extends Model
 {
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+    ];
+
     public function users()
     {
-      return $this->belongsToMany(User::class);
+        // Relación muchos a muchos con User
+        return $this->belongsToMany(User::class);
     }
-
 }
+
 
