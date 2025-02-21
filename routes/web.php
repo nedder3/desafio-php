2cd015b5ea2f5a2664a94e814d9bbac4b9f8ebcb
Nedder3 <arij.dev@gmail.com>
2025-02-21 03:38:59 -0300

    update with UserRoleController route

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\RoleController;
+use App\Http\Controllers\UserRoleController;
+
+
 
 Route::get('/', function () {
     return view('welcome');
@@ -16,7 +19,16 @@ Route::resource('users', UserController::class);
 Route::resource('roles', RoleController::class);
 
 //Ruta adicional para asignar roles a usuarios
-Route::post('users/{user}/roles', [UserController::class, 'assignRole'])
-    ->name('users.assignRole');
+//Route::post('users/{user}/roles', [UserController::class, 'assignRole'])
+  //->name('users.assignRole');
 
     
+    Route::resource('users.roles', UserRoleController::class)
+    ->except(['show','edit','update'])
+    // o ->only(['index','create','store','destroy']) si prefieres
+    ->names([
+        'index'  => 'users.roles.index',
+        'create' => 'users.roles.create',
+        'store'  => 'users.roles.store',
+        'destroy'=> 'users.roles.destroy',
+    ]);
