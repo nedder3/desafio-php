bd623d6f2580ae4025b4ed6e156f2db449d73fc4
Nedder3 <arij.dev@gmail.com>
2025-02-21 02:23:04 -0300

    add routers for users,roles and role-user assignation

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,20 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\RoleController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+//Rutas para Users (CRUD)
+Route::resource('users', UserController::class);
+
+//Rutas para Roles (CRUD)
+Route::resource('roles', RoleController::class);
+
+//Ruta adicional para asignar roles a usuarios
+Route::post('users/{user}/roles', [UserController::class, 'assignRole'])
+    ->name('users.assignRole');
